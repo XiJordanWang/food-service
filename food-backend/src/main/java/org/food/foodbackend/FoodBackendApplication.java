b8be35bdb058b7cbb94cdbf1441b69bfc59c3c20
XiJordanWang <wang.xi.jordan@gmail.com>
2026-03-28 13:03:27 +0800

    Initialize backend

---
+++ food-backend/src/main/java/org/food/foodbackend/FoodBackendApplication.java
@@ -0,0 +1,13 @@
+package org.food.foodbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoodBackendApplication.class, args);
+    }
+
+}
---
+++ food-backend/src/test/java/org/food/foodbackend/FoodBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.food.foodbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
